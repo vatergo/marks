e3d5d9a2dd41510eeb212a1c5ad05795dd01986d
vatergo <vatergo@mail.ru>
2019-06-16 21:34:24 +0500

    Updates

---
+++ precache-manifest.fd8ff06ae62fca2484ddcb6f76712e75.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27485c68e31c442ecbad34d66ea01acb",
+    "url": "/marks/index.html"
+  },
+  {
+    "revision": "9c1529b06be6cce61b73",
+    "url": "/marks/static/css/main.6183de88.chunk.css"
+  },
+  {
+    "revision": "32e50f3f89a714a99eb1",
+    "url": "/marks/static/js/2.48eeb91e.chunk.js"
+  },
+  {
+    "revision": "9c1529b06be6cce61b73",
+    "url": "/marks/static/js/main.0c3a8b86.chunk.js"
+  },
+  {
+    "revision": "cac7c21caa7cbfa8aa04",
+    "url": "/marks/static/js/runtime~main.1fe198c4.js"
+  }
+]);
